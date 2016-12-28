673582311520bfc5f8c7553514e03a00ad48c08e
Debaditya Chakravorty <debaditya.chakravorty@gmail.com>
2016-12-27 22:37:15 -0500

    Linear search
    Linear search

---
+++ linearsearch.scala
@@ -0,0 +1,29 @@
+import scala.util.control.Breaks._
+
+object linearsearch {
+  
+  def main (args : Array[String])
+  {
+    var arr = Array(3,41,24,1,2)
+    println("Enter the search no : ")
+    var search = Console.readInt()
+    
+    linearsearch(arr,search)
+
+  }
+  
+  def linearsearch (arr : Array[Int],search : Int)
+  {
+    breakable {
+    for (i <- 0 to arr.length)
+    {
+      if (arr(i) == search)
+      {
+          println("No found in " + (i+1) + " position")  
+          break
+      }
+    }
+    
+    }
+  }
+}
